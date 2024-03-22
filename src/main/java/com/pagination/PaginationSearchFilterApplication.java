dc6b2b23891f2eba2ff81d590de9f185b6389556
zees007 <zees007@rediffmail.com>
2024-03-22 23:35:36 +0530

    initial commit

---
+++ src/main/java/com/pagination/PaginationSearchFilterApplication.java
@@ -0,0 +1,13 @@
+package com.pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationSearchFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationSearchFilterApplication.class, args);
+	}
+
+}
